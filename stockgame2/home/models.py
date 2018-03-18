46aaf6d8ceb1ebd637e8c3a68df27484199d7287
Avanish Mishra <avanish.mishra@rutgers.edu>
2018-03-18 12:27:59 -0400

    Connecting to database and initializing models work http://killtheyak.com/use-postgresql-with-django-flask/ https://docs.djangoproject.com/en/2.0/topics/db/queries/

--- stockgame2/home/models.py
+++ stockgame2/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	userID = models.IntegerField()
+	username = models.CharField(max_length=20)
+        password = models.CharField(max_length=20)
+        email = models.CharField(max_length=40)
+        leagueID0 = models.IntegerField()
+        leagueID1 = models.IntegerField()
+        leagueID2 = models.IntegerField()
+        leagueID3 = models.IntegerField()
+
+class Setting(models.Model):
+
+	
+class Player(models.Model):
+
+
+class League(models.Model):
+
+
+class Transaction(models.Model):
+
+
+class Asset(models.Model):
+		
