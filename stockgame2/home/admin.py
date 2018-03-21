bd78494d69fa95e8618fc7d6ad4e456cd8429e52
safashaikh <safa.shaikh97@gmail.com>
2018-03-21 13:02:50 -0400

    registered models in admin.py

--- stockgame2/home/admin.py
+++ stockgame2/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Player, Setting, League, Transaction, Asset
 
+admin.site.register(User)
+admin.site.register(Player)
+admin.site.register(Setting)
+admin.site.register(League)
+admin.site.register(Transaction)
+admin.site.register(Asset)
 # Register your models here.
