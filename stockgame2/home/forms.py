40d478caae9cbb439d05c43fcbd63bcc7d674085
safashaikh <safa.shaikh97@gmail.com>
2018-03-25 09:38:07 -0400

    signup working according to django users

--- stockgame2/home/forms.py
+++ stockgame2/home/forms.py
@@ -3,20 +3,23 @@ from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator
 from django.utils.translation import gettext_lazy as _
 from django.core import validators
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class UserForm(forms.Form):
-	username = forms.CharField(label='Your name', max_length=20)
-	password = forms.CharField(max_length=20)
-	conf_pwd = forms.CharField(max_length=20)
-	email = forms.CharField(max_length=40, validators=[validators.validate_email])
-
+class SignUpForm(UserCreationForm):
+	# username = forms.CharField(label='Your name', max_length=20)
+	# password = forms.CharField(max_length=20)
+	# conf_pwd = forms.CharField(max_length=20)
+	email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+	class Meta:
+		model = User
+		fields = ('username', 'password1', 'password2', 'email')
 class LeagueForm(forms.Form):
 	isUniversal = forms.BooleanField()
 	numPlayers = forms.IntegerField()
 	beginDate = forms.DateTimeField()
 	endDate = forms.DateTimeField()
 	startingBalance = forms.DecimalField(decimal_places=2,max_digits=40)
-	adminID = forms.IntegerField()
 	isCrypto = forms.BooleanField()
 	joinPassword = forms.CharField(max_length=20)
 
