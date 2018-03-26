ff0378fef0512e814fd7ba3c7ae3634c04676ca3
safashaikh <safa.shaikh97@gmail.com>
2018-03-26 10:05:40 -0400

    fixed db will have to run few more tests

--- stockgame2/home/admin.py
+++ stockgame2/home/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User, Player, Setting, League, Transaction, Asset
+from .models import User, Player, League, Transaction, Asset
 
 admin.site.register(User)
 admin.site.register(Player)
-admin.site.register(Setting)
 admin.site.register(League)
 admin.site.register(Transaction)
 admin.site.register(Asset)
