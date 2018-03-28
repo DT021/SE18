3f9add1b725faa4764e7a8894b9bacef12835b6b
safashaikh <safa.shaikh97@gmail.com>
2018-03-28 01:21:17 -0400

    adding league test

--- stockgame2/home/tests.py
+++ stockgame2/home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import League
 
+class LeagueTestCase(TestCase):
+	def setUp(self):
+		League.objects.create(name="testLeague", isUniversal=False,numPlayers=15,beginDate=datetime.now,endDate = datetime.now()+datetime.timedelta(days=1),startingBalance = 25.36,adminID = 3, isCrypto = True, joinPassword = "pwd")
+
+	def test_LeagueCreated(self):
+		"""Leagues are correctly identified"""
+		league1 = League.objects.get(name="testLeague")
+		self.assertEqual(league1.isUniversal, False)
 # Create your tests here.
