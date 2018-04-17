7c01b2a16272c6add9124fa6ffec5cdb1dbe3ab0
Avanish Mishra <avanish.mishra@rutgers.edu>
2018-04-16 21:04:15 -0400

    Added profile model to models.py

--- stockgame2/home/models.py
+++ stockgame2/home/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.conf import settings
 from datetime import datetime  
 from django.contrib.auth.models import User as auth_User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
-# class User(models.Model):
-	# username = models.CharField(max_length=20)
-	# password = models.CharField(max_length=20)
-	# email = models.CharField(max_length=40)
-
-
+class Profile(models.Model):
+	user = models.OneToOneField(auth_User, on_delete=models.CASCADE)
+	trophies = ArrayField(models.IntegerField(), size = 8)
+	statement = models.CharField(max_length=250)
+	name = models.CharField(max_length=40)
+	birthday = models.DateTimeField()
+	
 class League(models.Model):
 	name = models.CharField(max_length=50)
 	isUniversal = models.BooleanField()
