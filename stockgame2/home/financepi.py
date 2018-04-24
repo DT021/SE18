b6814c18ecd7d1778a9076713464d83e5400b23c
Nick Lurski <nlurski@gmail.com>
2018-04-24 14:32:07 -0400

    make finance more robust

--- stockgame2/home/financepi.py
+++ stockgame2/home/financepi.py
@@ -14,11 +14,45 @@ def getPriceFromAPI(symbol, isCrypto):
 	}
 
 	df = get_price_data(param)
-	if df.empty:
-		return -1
-	price = df['Open'][-1]
-	return decimal.Decimal(price)
+	if !df.empty:
+		price = df['Open'][-1]
+		return decimal.Decimal(price)
+	param = {
+	'q': symbol, # Stock symbol (ex: "AAPL")
+	'i': "86400", # Interval size in seconds ("86400" = 1 day intervals)
+	'x': "INDEXDJX", # Stock exchange symbol on which stock is traded (ex: "NASD")
+	'p': "1Y" # Period (Ex: "1Y" = 1 year)
+	}
 
+	df = get_price_data(param)
+	if !df.empty:
+		price = df['Open'][-1]
+		return decimal.Decimal(price)
+		
+	param = {
+	'q': symbol, # Stock symbol (ex: "AAPL")
+	'i': "86400", # Interval size in seconds ("86400" = 1 day intervals)
+	'x': "INDEXSP", # Stock exchange symbol on which stock is traded (ex: "NASD")
+	'p': "1Y" # Period (Ex: "1Y" = 1 year)
+	}
+
+	df = get_price_data(param)
+	if !df.empty:
+		price = df['Open'][-1]
+		return decimal.Decimal(price)
+		
+	param = {
+	'q': symbol, # Stock symbol (ex: "AAPL")
+	'i': "86400", # Interval size in seconds ("86400" = 1 day intervals)
+	'x': "INDEXNYSEGIS", # Stock exchange symbol on which stock is traded (ex: "NASD")
+	'p': "1Y" # Period (Ex: "1Y" = 1 year)
+	}
+
+	df = get_price_data(param)
+	if !df.empty:
+		price = df['Open'][-1]
+		return decimal.Decimal(price)
+	return -1
 #print(getPriceFromAPI('GOOGL', False))
 
 def getCryptoPriceFromAPI(symbol, isCrypto):
