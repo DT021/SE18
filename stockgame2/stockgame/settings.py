5abcb28c1f2f5d3e6e0cc1ab74b72e065c9f18a8
Nick Lurski <nlurski@gmail.com>
2018-05-01 10:14:04 -0400

    debug = true

--- stockgame2/stockgame/settings.py
+++ stockgame2/stockgame/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=f7jz67*()7t+14gzbf&$5qer(&bhki+o7rtzlq(lsxu_r&o2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
